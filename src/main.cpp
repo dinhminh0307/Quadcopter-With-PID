a8f66f94b2d7b9f610135af74f0c3e6c3bfb5941
minhxiro <94245285+minhxiro@users.noreply.github.com>
2023-12-20 01:23:19 +0700

    push

--- src/main.cpp
+++ src/main.cpp
@@ -3,68 +3,147 @@
  */
 
 // #include <Drone.h>
+#if 1
 #include <Web.h>
 void setup()
 {
   Serial.begin(115200);
   initMPU();
-  initSPIFFS();
-  initWiFi();
-  webServerHandler();
-  eventHandler();
   //droneConfig();
   // Serial.println("setup");
 }
 
 void loop()
 {
-  sendEvent();
+  Serial.print(getGyroReadings().GyroX);
+  Serial.print("  ");
+  Serial.print(getGyroReadings().GyroY);
+  Serial.print("  ");
+  Serial.println(getGyroReadings().GyroZ);
+  delay(100);
   //rotateBLDC();
 }
-// #if 0
-// #include "./../lib/Drone/Drone.h"
-// #include <MPU.h>
-// #include <math.h>
-// float kp=.5;
-// float ki=55;
-// float kd=.00001;
-
-// Servo rollServo;
-// int milliOld;
-// int milliNew;
-// int dt;
-
-// float rollTarget=0;
-// float rollActual;
-// float rollError=0;
-// float rollErrorOld;
-// float rollErrorChange;
-// float rollErrorSlope=0;
-// float rollErrorArea=0;
-// float rollServoVal;
-
-// void setup() {
-//   initMPU();
-//   milliNew=millis(); // inital time
-// }
-
-// void loop() {
-//   rollActual = gyroDataSent.GyroX;
-//   milliOld=milliNew;
-//   milliNew=millis();
-//   dt=milliNew-milliOld;
-
-//   rollErrorOld=rollError;
-//   rollError=rollTarget-rollActual;
-//   rollErrorChange=rollError-rollErrorOld;
-//   rollErrorSlope=rollErrorChange/dt;
-//   rollErrorArea=rollErrorArea+rollError*dt;
-//   rollServoVal=rollServoVal+kp*rollError+ki*rollErrorSlope+kd*rollErrorArea;
-//   rollServo.write(rollServoVal);
-// }
-// #endif
+#endif
 
 #if 0
+#include <WiFi.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+#include <Arduino_JSON.h>
+#include "SPIFFS.h"
+// Replace with your network credentials
+const char *ssid = "DongLao";
+const char *password = "Ntn.1991";
+// Create AsyncWebServer object on port 80
+AsyncWebServer server(80);
+
+// Create an Event Source on /events
+AsyncEventSource events("/events");
+
+// Json Variable to Hold Sensor Readings
+JSONVar readings;
+
+// Timer variables
+unsigned long lastTime = 0;  
+unsigned long lastTimeTemperature = 0;
+unsigned long lastTimeAcc = 0;
+unsigned long gyroDelay = 10;
+unsigned long temperatureDelay = 1000;
+unsigned long accelerometerDelay = 200;
+
+// Create a sensor object
+Adafruit_MPU6050 mpu;
+
+sensors_event_t a, g, temp;
+
+float gyroX, gyroY, gyroZ;
+float accX, accY, accZ;
+float temperature;
+
+//Gyroscope sensor deviation
+float gyroXerror = 0.07;
+float gyroYerror = 0.03;
+float gyroZerror = 0.01;
+
+// Init MPU6050
+void initMPU(){
+  if (!mpu.begin()) {
+    Serial.println("Failed to find MPU6050 chip");
+    while (1) {
+      delay(10);
+    }
+  }
+  Serial.println("MPU6050 Found!");
+}
+
+void initSPIFFS() {
+  if (!SPIFFS.begin()) {
+    Serial.println("An error has occurred while mounting SPIFFS");
+  }
+  Serial.println("SPIFFS mounted successfully");
+}
+
+// Initialize WiFi
+void initWiFi() {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.println("");
+  Serial.print("Connecting to WiFi...");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(1000);
+  }
+  Serial.println("");
+  Serial.println(WiFi.localIP());
+}
+
+String getGyroReadings(){
+  mpu.getEvent(&a, &g, &temp);
+
+  float gyroX_temp = g.gyro.x;
+  if(abs(gyroX_temp) > gyroXerror)  {
+    gyroX += gyroX_temp/50.00;
+  }
+  
+  float gyroY_temp = g.gyro.y;
+  if(abs(gyroY_temp) > gyroYerror) {
+    gyroY += gyroY_temp/70.00;
+  }
+
+  float gyroZ_temp = g.gyro.z;
+  if(abs(gyroZ_temp) > gyroZerror) {
+    gyroZ += gyroZ_temp/90.00;
+  }
+
+  readings["gyroX"] = String(gyroX);
+  readings["gyroY"] = String(gyroY);
+  readings["gyroZ"] = String(gyroZ);
+
+  String jsonString = JSON.stringify(readings);
+  return jsonString;
+}
+
+String getAccReadings() {
+  mpu.getEvent(&a, &g, &temp);
+  // Get current acceleration values
+  accX = a.acceleration.x;
+  accY = a.acceleration.y;
+  accZ = a.acceleration.z;
+  readings["accX"] = String(accX);
+  readings["accY"] = String(accY);
+  readings["accZ"] = String(accZ);
+  String accString = JSON.stringify (readings);
+  return accString;
+}
+
+String getTemperature(){
+  mpu.getEvent(&a, &g, &temp);
+  temperature = temp.temperature;
+  return String(temperature);
+}
+
 void setup() {
   Serial.begin(115200);
   initWiFi();
@@ -117,7 +196,7 @@ void setup() {
 void loop() {
   if ((millis() - lastTime) > gyroDelay) {
     // Send Events to the Web Server with the Sensor Readings
-    events.send(getGyroWeb().c_str(),"gyro_readings",millis());
+    events.send(getGyroReadings().c_str(),"gyro_readings",millis());
     lastTime = millis();
   }
   if ((millis() - lastTimeAcc) > accelerometerDelay) {
