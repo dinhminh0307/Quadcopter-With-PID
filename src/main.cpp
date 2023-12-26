971a9aa4866de060de9a34fe41a510f433a72bbd
CuongNguyen-rmit <s3891758@rmit.edu.vn>
2023-12-26 16:50:36 +0700

    commit

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@
 // #include <GoogleSheet.h>
 #include <PID.h>
 
-// void tunningPID();
+void tunningPID();
 
 void setup()
 {
@@ -16,22 +16,19 @@ void setup()
   Init_ESPnow();
   Init_ESC();
   Init_MPU();
-  Init_Serial2(); // for the gps
-  // Init_GoogleSheet();
+  Init_PID();
 
-  // Init_PID();
+  // Init_GoogleSheet();
 }
 
 void loop()
 {
-
-  // Compute_PID();
   Get_MPUangle();
   Compute_PID();
   rotateBLDC();
   // recordGPStoGoogleSheet();
   SerialDataPIDPrint();
-  // tunningPID();
+  tunningPID();
   // Serial.println("in loop");
 }
 
@@ -41,38 +38,38 @@ void loop()
 // To change the I value to -5, type i-5
 // To change the D value to 2.4, type d2.4
 // To change the setpoint to 3, type s3
-// void tunningPID()
-// {
-//   static String received_chars;
-//   Serial.println("Please select tunning option");
-//   while (!Serial.available())
-//   {
-//     char inChar = (char)Serial.read();
-//     received_chars += inChar;
-//     if (inChar == '\n')
-//     {
-//       switch (received_chars[0])
-//       {
-//       case 'p':
+void tunningPID()
+{
+  static String received_chars;
+  Serial.println("Please select tunning option");
+  while (!Serial.available())
+  {
+    char inChar = (char)Serial.read();
+    received_chars += inChar;
+    if (inChar == '\n')
+    {
+      switch (received_chars[0])
+      {
+      case 'p':
 
-//         received_chars.remove(0, 1);
-//         kp = received_chars.toFloat();
-//         break;
-//       case 'i':
-//         received_chars.remove(0, 1);
-//         ki = received_chars.toFloat();
-//         break;
-//       case 'd':
-//         received_chars.remove(0, 1);
-//         kd = received_chars.toFloat();
-//         break;
-//       case 's':
-//         received_chars.remove(0, 1);
-//         anglex_setpoint = received_chars.toFloat();
-//       default:
-//         break;
-//       }
-//       received_chars = "";
-//     }
-//   }
-// }
+        received_chars.remove(0, 1);
+        kp = received_chars.toFloat();
+        break;
+      case 'i':
+        received_chars.remove(0, 1);
+        ki = received_chars.toFloat();
+        break;
+      case 'd':
+        received_chars.remove(0, 1);
+        kd = received_chars.toFloat();
+        break;
+      case 's':
+        received_chars.remove(0, 1);
+        anglex_setpoint = received_chars.toFloat();
+      default:
+        break;
+      }
+      received_chars = "";
+    }
+  }
+}
